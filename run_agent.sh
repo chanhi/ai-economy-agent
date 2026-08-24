dd06084478dd31dfb05c71dd0d770e4cfd0d1075
chanhi <chanhi421@naver.com>
2026-08-24 09:52:05 +0900

    fix(cron) 경로 문제 수정

--- run_agent.sh
+++ run_agent.sh
@@ -4,10 +4,10 @@ echo "========================================"
 echo "실행 시간: $(date +'%Y-%m-%d %H:%M:%S')"
 echo "========================================"
 
-cd /home/pi/economySummary
+cd /home/chan/economySummary
 
 # 파이썬 실행
-/home/pi/.local/bin/uv run main.py
+/home/chan/.local/bin/uv run main.py
 
 # 깃허브 업로드 (옵시디언 동기화를 나중에 하신다고 했지만, 코드를 백업용으로 남겨둡니다)
 git add obsidian_notes/*.md
